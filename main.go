97e2fce93ff4fb9e0d6d0259db8ce5f986d9f233
Tony Mackay <hello@tonymackay.net>
2020-08-31 12:21:43 +0100

    Change output of command to show outfile paths

--- main.go
+++ main.go
@@ -89,7 +89,7 @@ func dirents(dir string) []os.FileInfo {
 func cjpeg(path string, quality int64) error {
 	// replace ext with jpg for output path
 	ext := filepath.Ext(path)
-	name := filepath.Base(path)
+	//name := filepath.Base(path)
 	outfile := path[0:len(path)-len(ext)] + ".jpg"
 	q := strconv.FormatInt(quality, 10)
 	cmd := exec.Command("cjpeg", "-quality", q, "-optimize", "-progressive", "-outfile", outfile, path)
@@ -101,6 +101,6 @@ func cjpeg(path string, quality int64) error {
 		return err
 	}
 
-	fmt.Printf("converted: %s\n", name)
+	fmt.Printf("converted: %s to: %s\n", path, outfile)
 	return nil
 }
